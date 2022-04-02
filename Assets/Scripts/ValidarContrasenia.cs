af7a919e3e1785692bc7a093278221670285dc56
FrancoSandri <46960579@est.ort.edu.ar>
2022-04-02 13:10:12 -0300

    TP-02 completed

--- Assets/Scripts/ValidarContrasenia.cs
+++ Assets/Scripts/ValidarContrasenia.cs
@@ -8,11 +8,14 @@ public class ValidarContrasenia : MonoBehaviour
     string contraseniaCorrecta;
     string contraseniaUsuario;
     public Text ingresoUsuario;
+    public GameObject cartelitoMsj;
+    public Text textoMsj;
 
     // Start is called before the first frame update
     void Start()
     {
         contraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -26,11 +29,15 @@ public class ValidarContrasenia : MonoBehaviour
         contraseniaUsuario = ingresoUsuario.text;
         if(contraseniaUsuario == contraseniaCorrecta)
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
             
         }
         else
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña Incorrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
