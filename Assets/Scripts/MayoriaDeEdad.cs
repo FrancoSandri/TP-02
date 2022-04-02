31f3005dd264fed1735356db5be3de623205a873
FrancoSandri <46960579@est.ort.edu.ar>
2022-04-02 11:45:36 -0300

    Cambiar texto UI por codigo completed

--- Assets/Scripts/MayoriaDeEdad.cs
+++ Assets/Scripts/MayoriaDeEdad.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MayoriaDeEdad : MonoBehaviour
 {
     public int edadUsuario;
+    public Text miTexto;
     // Start is called before the first frame update
 
     void Start()
@@ -12,9 +14,11 @@ public class MayoriaDeEdad : MonoBehaviour
 
         if (edadUsuario >= 18)
         {
+            miTexto.text = "Es mayor de edad";
             Debug.Log("Es mayor de edad");
         }else
         {
+            miTexto.text = "No es mayor de edad";
             Debug.Log("No es mayor de edad");
         }
     }
